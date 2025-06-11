a5a18895e3b52f9f00855c23c864cbde23d7195b
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-06-11 09:45:12 +0300

    Update App.tsx
    used interface to add component properties

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
+interface MyButtonProperties {
+  /** The text to display inside the button */
+  title: string;
+  /** Whether the button can be interacted with */
+  disabled: boolean;
+}
+
 function MyButton({title}: {title: string}) {
   return (
     <button>{title}</button>
+  );
+}
+
+function MySecondButton({title, disabled}: MyButtonProperties) {
+  return (
+    <button disabled={disabled}>{title}</button>
   );
 }
 
@@ -10,6 +23,8 @@ export default function App() {
         <div>
           <h1>My First React App</h1>
           <MyButton title="Click Me!"/>
+          <br /><br />
+          <MySecondButton disabled={true} title="Can't Click Me!"/>
         </div>
       </>
   );
